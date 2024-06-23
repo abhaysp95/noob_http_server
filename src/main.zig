f513823e9b8ff7e4e8b78b557b1a8e5f770aa4d8
abhaysp95 <abhaysp95@protonmail.com>
2024-06-23 22:16:06 +0530

    feat: seperate function for request parsing and making & sending response
        reading request header and setting body accordingly

--- src/main.zig
+++ src/main.zig
@@ -24,17 +24,19 @@ pub fn main() !void {
     try stdout.print("client connected!\n", .{});
 
     const req = try parse_request(&connection, allocator);
-    try stdout.print("status: {s}\n", .{req.status});
+    const resp = try make_response(&req, allocator);
+    try send_resp(&connection, &resp);
 
-    var header_iterator = req.headers.?.iterator();
-    while (header_iterator.next()) |entry| {
-        try stdout.print("{s}: {s}\n", .{ entry.key_ptr.*, entry.value_ptr.* });
+    try stdout.print("status line: {s}\n", .{resp.status});
+    if (resp.headers) |headers| {
+        var header_iter = headers.iterator();
+        while (header_iter.next()) |entry| {
+            try stdout.print("{s}: {s}\r\n", .{ entry.key_ptr.*, entry.value_ptr.* });
+        }
     }
-    if (req.body) |body| {
-        try stdout.print("body: {s}\n", .{body});
+    if (resp.body) |body| {
+        try stdout.writeAll(body);
     }
-
-    _ = try connection.stream.write("HTTP/1.1 200 OK\r\n\r\n");
 }
 
 fn parse_request(conn: *const Connection, allocator: std.mem.Allocator) !Request {
@@ -70,4 +72,70 @@ fn parse_request(conn: *const Connection, allocator: std.mem.Allocator) !Request
         .headers = headers,
         .body = body,
     };
+}
+
+fn make_response(req: *const Request, allocator: std.mem.Allocator) !Response {
+    var req_status_iter = std.mem.splitSequence(u8, req.status, " ");
+    _ = req_status_iter.next(); // no need for req HTTP verb
+    const endpoint = req_status_iter.next().?;
+
+    var headers = HashMap.init(allocator);
+    if (std.mem.eql(u8, endpoint, "/")) {
+        try headers.put("Content-Length", try std.fmt.allocPrint(allocator, "0", .{}));
+        return .{
+            .status = "HTTP/1.1 200 OK\r\n\r\n",
+            .headers = headers,
+            .body = null,
+        };
+    } else if (std.mem.eql(u8, endpoint, "/user-agent")) {
+        try headers.put("Content-Type", "text/plain");
+        try headers.put("Content-Length", try std.fmt.allocPrint(allocator, "{d}", .{req.headers.?.get("User-Agent").?.len}));
+        return .{
+            .status = "HTTP/1.1 200 OK\r\n\r\n",
+            .headers = headers,
+            .body = try std.fmt.allocPrint(allocator, "{s}", .{req.headers.?.get("User-Agent").?}),
+        };
+    } else if (!std.mem.startsWith(u8, endpoint, "/echo")) {
+        try headers.put("Content-Length", try std.fmt.allocPrint(allocator, "0", .{}));
+        return .{
+            .status = "HTTP/1.1 404 Not Found\r\n\r\n",
+            .headers = headers,
+            .body = null,
+        };
+    } else {
+        // split to get endpoint heirarchy
+        var target_level_iter = std.mem.tokenizeSequence(u8, endpoint, "/");
+        var resource: []const u8 = undefined;
+        while (target_level_iter.next()) |res| {
+            resource = res;
+        }
+
+        try headers.put("Content-Type", "text/plain");
+        try headers.put("Content-Length", try std.fmt.allocPrint(allocator, "{d}", .{resource.len}));
+        return .{
+            .status = "HTTP/1.1 200 OK\r\n",
+            .headers = headers,
+            .body = try std.fmt.allocPrint(allocator, "{s}", .{resource}),
+        };
+    }
+}
+
+fn send_resp(conn: *const Connection, resp: *const Response) !void {
+    const writer = conn.stream.writer();
+    const write_len = try writer.write(resp.status);
+    if (write_len < resp.status.len) {
+        // TODO: error handling here
+    }
+
+    if (resp.headers) |headers| {
+        var header_iter = headers.iterator();
+        while (header_iter.next()) |entry| {
+            try writer.print("{s}: {s}\r\n", .{ entry.key_ptr.*, entry.value_ptr.* });
+        }
+        _ = try writer.write("\r\n"); // mark ending of headers
+    }
+
+    if (resp.body) |body| {
+        try writer.writeAll(body);
+    }
 }
